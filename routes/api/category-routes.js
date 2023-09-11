c78d62be1968ba31276584c8d01fbdbe76b0f5ac
evan-pieper <evanjpieper@gmail.com>
2023-09-11 14:54:54 -0700

    added post route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,11 +30,18 @@ router.get('/:id', (req, res) => {
 	});
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
-  Category.create(req.body).then((category) => {
-    res.json(category);
-  });
+  //Category.create(req.body).then((category) => {
+    //res.json(category);
+  //});
+	try{
+		const newCategory = await Category.create({
+			category_name: req.body.category_name
+		});
+		res.status(200).json(newCategory);
+	} catch (err) {res.status(500).json(err);}
+
 });
 
 router.put('/:id', (req, res) => {
