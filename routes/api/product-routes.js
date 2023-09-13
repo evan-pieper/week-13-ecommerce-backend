92bb77fccfc677f260729926d209c3553f91bc3d
evan-pieper <evanjpieper@gmail.com>
2023-09-12 20:29:45 -0700

    implemented restful delete method for product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -101,8 +101,16 @@ router.put('/:id', (req, res) => {
 		});
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
 	// delete one product by its `id` value
+	try{
+		Product.destroy({
+			where: {
+				id: req.params.id, //gets the id from the url
+			},
+		});
+		res.status(200).json({message: "Product deleted."});
+	} catch (err) {res.status(500).json(err)}; //catches any errors and returns them
 });
 
 module.exports = router;
